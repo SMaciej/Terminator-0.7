d62eae995e69b2c267589a217acd0cc107da6490
Sommyboy <sssom3@gmail.com>
2017-01-08 00:30:49 +0100

    Lepsze wykrecanie

--- terminator/ultrasonic.ino
+++ terminator/ultrasonic.ino
@@ -46,41 +46,23 @@ Serial.println("oryginalny print");
   fuzzy->setInput(3, limit(rightEye.ping_cm(),3)); // right
   fuzzy->fuzzify();
 
-  
-  Serial.println("Zasady:");
-  bool wasTheRulleFired = fuzzy->isFiredRule(1);
-  Serial.println(wasTheRulleFired);
-  wasTheRulleFired = fuzzy->isFiredRule(2);
-  Serial.println(wasTheRulleFired);
-  wasTheRulleFired = fuzzy->isFiredRule(3);
-  Serial.println(wasTheRulleFired);
-  wasTheRulleFired = fuzzy->isFiredRule(4);
-  Serial.println(wasTheRulleFired);
-  wasTheRulleFired = fuzzy->isFiredRule(5);
-  Serial.println(wasTheRulleFired);
-  wasTheRulleFired = fuzzy->isFiredRule(6);
-  Serial.println(wasTheRulleFired);
-  wasTheRulleFired = fuzzy->isFiredRule(7);
-  Serial.println(wasTheRulleFired);
-  wasTheRulleFired = fuzzy->isFiredRule(8);
-  Serial.println(wasTheRulleFired);
-  wasTheRulleFired = fuzzy->isFiredRule(9);
+  bool wasTheRulleFired = fuzzy->isFiredRule(2);
   Serial.println(wasTheRulleFired);
   
-
+  
   int output = fuzzy->defuzzify(1);
   int output2 = fuzzy->defuzzify(2);
   int output3 = fuzzy->defuzzify(3);
 
 
   //Serial.println(output2);
-  Serial.println("Deffuzify");
+  Serial.println("Output3");
   Serial.println(output);
   Serial.println(output3);
 
   if ( output3 >= 5)
   {
-    Serial.println("Prawo o mocy:");
+    Serial.println("Lewo o mocy:");
     Serial.println(output2);
     motorsTurnRight(output2,125);
   }
@@ -88,24 +70,70 @@ Serial.println("oryginalny print");
   {
     if( output3 > 0)
     {
-      Serial.println("Lewo o mocy:");
+      Serial.println("Prawo o mocy:");
       Serial.println(output2);
       motorsTurnLeft(output2,125);
     }
     else
     {
-      if(output > 170)
-      {
-        motorsForward(80,output);  
-      }
-      else
-      {
-        motorsForward(60,output);
-      }
+      motorsForward(50,output);
     }
   }
-  //Serial.println(output2);
 
+  
+  /*
+  Serial.println(output);
+  if (output2 >= 1000)
+  {
+    output2 = output2 - 900;
+    Serial.println("Skrecilem w lewo!");
+  }
+  else
+  { 
+    if (output2 > 0)
+    {
+      Serial.println("Skrecilem w prawo!");
+    }
+  }
+  */
+  /*
+  if (output2 >= 1000)
+  {
+    Serial.println("lewo");
+    output2 = output2 - 750;
+  }
+  else
+  {
+    if (output2 > 0)
+    {
+      Serial.println("prawo");
+      output2 = output2 - 450;
+    }
+  }
+  Serial.println(output2);
+  */
+  /*
+  if(output2 == 0 )
+  {
+    
+    Serial.println("Wykonalem jazde do przodu o mocy ");
+    //Serial.print(output);
+    if(output > 170)
+    {
+      motorsForward(75,output);
+    }
+    else
+    {
+      motorsForward(50,output);
+    }
+  }
+  else
+  {
+    //Serial.println(output2);
+    Serial.println("wykonalem skret w lewo");
+    motorsTurnLeft(output2,125);
+  }
+  */
   //delay(1000);
   
 }
